50bde5fbcd8a22c621ea11dc0450fbb23784490d
Thunder Shiviah <thunder@uoregon.edu>
2014-06-29 22:42:55 -0700

    added main script

---
+++ workspace/main.py
@@ -0,0 +1,17 @@
+"""Script to gather and plot average rgb values from amazon book covers."""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+def load_json_file():
+    """Loads the scraped json file of amazon book information"""
+    pass
+
+def get_avg_rgb(url):
+    """Gets average rgb values from book covers by querying a color API"""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
